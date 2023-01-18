7dcf3d2820fa89a8433ec280d48d6c7eb3b5b3bb
Yuki Muto <yuki@Yukis-Mac-mini.local>
2023-01-18 12:15:06 -0800

    create xcode default project

---
+++ MemoryGameDemo/MemoryGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameDemoApp.swift
+//  MemoryGameDemo
+//
+//  Created by Yuki Muto on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
