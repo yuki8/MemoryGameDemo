8eb777ef33f58431500da56e2835ae4c6eec02fe
minipower <ymuto@hotmail.com>
2023-02-06 15:58:23 -0800

    add init to AspectVGrid Viewand also add more comments

--- MemoryGameDemo/AspectVGrid.swift
+++ MemoryGameDemo/AspectVGrid.swift
@@ -3,7 +3,7 @@
 //  MemoryGameDemo
 //
 //  Created by Yuki Muto on 2/5/23.
-//
+//         -- Originally written by CS193p Instructor (Stanford Univerity)
 
 import SwiftUI
 
@@ -12,10 +12,21 @@ struct AspectVGrid<Item, ItemView>: View where Item: Identifiable, ItemView: Vie
     var aspectRatio: CGFloat
     var content: (Item) -> ItemView
     
+    // content function passed here will "escape" (been stored) from init for later use
+    // @escaping keyword to let the compiler to know
+    // @ViewBuilder keyword to accept any passing function to be recognized as a View Builder
+    init(items: [Item], aspectRatio: CGFloat, @ViewBuilder content: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.aspectRatio = aspectRatio
+        self.content = content
+    }
+    
     var body: some View {
         GeometryReader { geometry in
+            // Make VStack flexible by adding Spacer, therefore GeometryReaer flexible too
             VStack {
                 let width: CGFloat = widthThatFits(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio)
+                // make sure the space between rows are zero for function widthThatFits to work
                 LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
                     ForEach(items) { item in
                         content(item).aspectRatio(aspectRatio, contentMode: .fit)
@@ -27,7 +38,7 @@ struct AspectVGrid<Item, ItemView>: View where Item: Identifiable, ItemView: Vie
         
     }
     
-    // Set horizontal spacing to zero
+    // make sure the space between columns are zero for function widthThatFits to work
     private func adaptiveGridItem(width: CGFloat) -> GridItem {
         var gridItem = GridItem(.adaptive(minimum: width))
         gridItem.spacing = 0
@@ -35,6 +46,7 @@ struct AspectVGrid<Item, ItemView>: View where Item: Identifiable, ItemView: Vie
     }
     
     // The function assumes no spacing between items
+    // -- Add padding to each item so that the function incorporates them before calculation
     private func widthThatFits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
         var columnCount = 1
         var rowCount = itemCount
